19db8b6c1b21dbfb6b9641546a929386fa7e05ec
valentinfuntak <valentinfuntakk@gmail.com>
2024-11-20 18:34:24 +0100

    konfiguracija

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `
-          @tailwind base;
-          @tailwind components;
-          @tailwind utilities;
-        `
-      }
-    }
-  },
   server: {
     port: 3000,
   },
